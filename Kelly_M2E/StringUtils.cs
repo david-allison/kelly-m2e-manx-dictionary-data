920912bfdc1dc146641b235276bb04057764d6d6
David Allison <62114487+david-allison@users.noreply.github.com>
2023-01-28 00:59:18 +0000

    fix more definitions
    mostly commas
    
    Handle ! in definition

--- Kelly_M2E/StringUtils.cs
+++ Kelly_M2E/StringUtils.cs
@@ -5,6 +5,6 @@ public static class StringUtils
     public static bool HasUpperCaseWordsGreaterThanLength1(this string s)
     {
         var entry = s.Split(" ");
-        return entry.Any(x => x.Length > 1 && x.Where(char.IsAscii).All(x => char.IsUpper(x) || x == '-'));
+        return entry.Any(x => x.Length > 1 && x.Where(char.IsAscii).All(x => char.IsUpper(x) || x == '-' || x == '!'));
     }
 }
