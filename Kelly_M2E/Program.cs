e1c653730c548bd44c22a98c9f0a53527bd5b55f
David Allison <62114487+david-allison@users.noreply.github.com>
2023-01-28 01:46:53 +0000

    add print function

--- Kelly_M2E/Program.cs
+++ Kelly_M2E/Program.cs
@@ -3,6 +3,14 @@
 using HtmlAgilityPack;
 using Kelly_M2E;
 
+void Print(IEnumerable<Headword> enumerable)
+{
+    foreach (var headword in enumerable)
+    {
+        Console.WriteLine(headword.HeadWords[0] + "\t" + headword.Entry);
+    }
+}
+
 string resourceName = "Fockleyr J Kelly m2e_CORR_clean.html";
 
 var docText = MSWord.LoadEncodedFile(resourceName);
@@ -28,13 +36,15 @@ var headwords = doc.DocumentNode.Descendants("p")
     .Select(x => x!)
     .ToList();
     
-var hasWordInDefinition = headwords.Where(x => x.Entry.HasUpperCaseWordsGreaterThanLength1());
+var hasWordInDefinition = headwords.Where(x => x.Entry.HasUpperCaseWordsGreaterThanLength1()).ToList();
 Console.WriteLine(hasWordInDefinition.Count() + " problems");
 
-var hasNoHeadWord = headwords.Where(x => x.HeadWords.Length == 0);
+var hasNoHeadWord = headwords.Where(x => x.HeadWords.Length == 0).ToList();
 Console.WriteLine(hasNoHeadWord.Count() + " problems");
 //
 // foreach (var word in headwords)
 // {
 //     Console.WriteLine(word.HeadWords[0] + "\t" + word.Entry);
 // }
+
+Print(headwords);
